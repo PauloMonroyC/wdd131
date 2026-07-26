1ea5f68e1a9e4d9b49b6641bf81631399d78062d
PauloMonroyC <pmonroycotrina@byupathway.edu>
2026-07-26 14:09:49 -0500

    Contry-Page

--- scripts/place.js
+++ scripts/place.js
@@ -2,3 +2,20 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
     `Last Modification: ${document.lastModified}`;
+
+const temperature = 6.5; // °C
+const windSpeed = 13; // km/h 
+
+function calculateWindChill(temp,wind) {
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16)).toFixed(1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const windChillElement = document.getElementById('windChillValue');
+
+    let windChillDisplay;
+
+    if (temperature <= 10 && windSpeed > 4.8) {windChillDisplay = `${calculateWindChill(temperature, windSpeed)}°C` ;} 
+        else {windChillDisplay = 'N/A';}
+        windChillElement.textContent = windChillDisplay;
+}); 
